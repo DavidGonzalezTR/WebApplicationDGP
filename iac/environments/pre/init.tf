53de02aaaef3c8eec2189cd3715dcf5e285cbb8d
DavidGonzalezTR <david.gonzalez@thomsonreuters.com>
2021-12-29 13:54:35 +0100

    PRE y PRO

---
+++ iac/environments/pre/init.tf
@@ -0,0 +1,20 @@
+# Configure min terraform version
+terraform {
+  required_version = ">=0.15.5"
+}
+# Initializing the module to connect to Azure
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
+}
+
+# This will configure Terraform to store the state file in a Azure Storage, so it can be shared
+# to everyone on the team. Please notice that we need to have separated storage for each environment we have (dev, qa, preprod, prod) for security reasons
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "uksouth-iflx-shar-dev-rg"
+    storage_account_name = "infolexterraformstatedev"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate.POC_ADO_PRE"
+  }
+}
